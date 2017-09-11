861167e795fb109253c32a9bfbd20b2a57e6fc77
supreet <supreet.shm523@gmail.com>
2017-09-11 11:08:15 +0530

    angular js services

--- 5. services/controller.js
+++ 5. services/controller.js
@@ -1,6 +1,6 @@
 var myNamespace = angular.module('myApp', []);
 myNamespace.controller('myController', function myController($scope, $http){
-	$http.get("../js/data.json").success(function(data){
-		$scope.author = data;
+	$http.get("data.json").success(function(data){
+		$scope.artist = data;
 	})
 	});
